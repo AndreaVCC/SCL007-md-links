247a136fe8740edc3564018975baae5c989273e0
Andrea Carreño <45242344+AndreaVCC@users.noreply.github.com>
2019-02-25 17:23:39 -0300

    Instalacion de markdownLinkExtractor para extraer los link, instalacion de node fetch

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-module.exports = () => {
-  // ...
-};
+
+const fs = require('fs');
+const markdownLinkExtractor = require('markdown-link-extractor');
+
+const fetch = require('node-fetch');
+
+const ruta = process.argv[2];
+
+var markdown = fs.readFileSync(ruta).toString();
+var links = markdownLinkExtractor(markdown);
+
+links.forEach(function (link) {
+    console.log(link);
+});
+
