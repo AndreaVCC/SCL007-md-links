22eb79f3b2f139f58921d1c93c089326f1bd2930
Andrea Carreño <45242344+AndreaVCC@users.noreply.github.com>
2019-02-27 16:07:34 -0300

    agregando path en index.js

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 
 const fs = require('fs');
+const path = require('path')
 const markdownLinkExtractor = require('markdown-link-extractor');
 const fetch = require('node-fetch'); //
-const ruta = process.argv[2];
 // process.argv[0] == 'node'
 // process.argv[1] == 'archivo js al que se refiere'
 // process.argv[2] == '1'
 // process.argv[3] == '2'
 // process.argv[4] == '3'
-
-var markdown = fs.readFileSync(ruta).toString();
-var links = markdownLinkExtractor(markdown);
+const ruta = process.argv[2];
+let urabsolute = path.resolve(ruta);
+const markdown = fs.readFileSync(urabsolute).toString();
+const links = markdownLinkExtractor(markdown);
 
 // links.forEach( link => {
 //     console.log(link);
 //  });
 
-
 //console.log(links) muestra arreglo con los links
+
+mdlinks = (h,v,s) => {
 
 const arrayFetch = []; 
 
@@ -29,16 +31,15 @@ for (let i = 0; i < links.length; i++) {
     .then(res => {
       const objectLinks = {
         href: `${res.url}`,
-        text: text || "",
+        text: text || "Sin texto",
         statusLink: `${res.status} ${res.statusText}`,     
       };
       return objectLinks;
     })
     .catch(err => {
       const objectFail = { 
-        //text: `${text}`,
         href: `${url}`,
-        Error: "Fail",
+        Error: "Link no disponible",
       };
       return objectFail;
     });
@@ -50,6 +51,7 @@ Promise.all(arrayFetch)
     console.log(arrayResp);
     });
 
-mdlink = (h,v,s) => {
+
 
 };
+mdlinks()
